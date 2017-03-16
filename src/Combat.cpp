cf6a75e6b70b2bb2832da3dcc088e8463d949a6f
p1510421 <devnull@localhost>
2017-03-16 17:28:44 +0100

    Début de Combat

--- src/Combat.cpp
+++ src/Combat.cpp
@@ -8,4 +8,47 @@
 
 using namespace std;
 
+	Combat::Combat(){
+		
+	}
+	
+	Combat::Combat(Joueur joueur_a, Joueur joueur_d, Region region_a, Region region_d){
+		attaquant = joueur_a;
+		defenseur = joueur_d;
+		region_attaquant = region_a;
+		region_defenseur = region_d;
+	}
+		
+	Combat::~Combat(){
+	
+	}
+	
+	Joueur Combat::get_attaquant() const{
+		return attaquant;
+	}
+	
+	void Combat::set_attaquant(Joueur att){
+				
+	}
+	
+	Joueur Combat::get_defenseur() const{
+		return defenseur;
+	}
 
+	void Combat::set_defenseur(Joueur def);
+
+	Region Combat::get_region_attaquant() const{
+		return region_attaquant;
+	}
+
+	void Combat::set_region_attaquant(Region region_a);
+	
+	Region Combat::get_region_defenseur() const{
+		return region_defenseur;
+	}
+
+	void Combat::set_region_defenseur(Region region_d);
+	
+	Joueur Combat::bataille(unsigned int nb_troupes_attaquant, unsigned int nb_troupes_defenseur);
+	
+	void Combat::maj_troupes(Region& region_attaquant, Region& region_defenseur);
--- src/Combat.h
+++ src/Combat.h
@@ -7,17 +7,67 @@
 #include <fstream>
 #include <cassert>
 
+#include "Joueur.h"
+#include "Region.h"
+
 using namespace std;
 
 class Combat {
 
 	private : 
-		//attaquant
-		//defenseur
-		//region
-		//region
+		Joueur attaquant;
+		Joueur defenseur;
+		Region region_attaquant;
+		Region region_defenseur;
 
-	//public :
+	public : 
+	/** @brief Constructeur par défaut */
+		Combat();
+		Combat(Joueur joueur_a, Joueur joueur_d, Region region_a, Region region_d);
+		
+	/** @brief Destructeur */
+		~Combat();
+	
+	/** @brief Accesseur pour le joueur attaquant 
+		@return Les informations concernant le joueur attaquant */
+	Joueur get_attaquant() const;
+	
+	/** @brief Mutateur pour le joueur attaquant
+		@param attaquant : Joueur */
+	void set_attaquant(Joueur att);
+	
+	
+	/** @brief Accesseur pour le joueur defenseur
+		@return Les informations concernant le joueur defenseur */
+	Joueur get_defenseur() const;
+	
+	/** @brief Mutateur pour le joueur defenseur
+		@param defenseur : Joueur */
+	void set_defenseur(Joueur def);
+	
+	
+	/** @brief Accesseur pour la region region_attaquant 
+		@return Les informations concernant la region region_attaquant */
+	Region get_region_attaquant() const;
+	
+	/** @brief Mutateur pour la region region_attaquant
+		@param region_attaquant : Region */
+	void set_region_attaquant(Region region_a);
+	
+	
+	/** @brief Accesseur pour la region_defenseur 
+		@return Les informations concernant la region_defenseur */
+	Region get_region_defenseur() const;
+	
+	/** @brief Mutateur pour la region_defenseur
+		@param region_defenseur : Region */
+	void set_region_defenseur(Region region_d);
+	
+	Joueur bataille(unsigned int nb_troupes_attaquant, unsigned int nb_troupes_defenseur);
+	
+	void maj_troupes(Region& region_attaquant, Region& region_defenseur);
+	
 };
+
 
 #endif
